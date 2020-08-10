3ddd157a934d91402d5896073b022ad7ed5cc4c7
tran thanh quang <quang.tran290699@gmail.com>
2020-08-10 17:46:32 +0700

    clone tocotoco

--- js/v3.js
+++ js/v3.js
@@ -5,6 +5,9 @@ let collectionCount = 0;
 const productHash = {};
 let cart = [];
 const products = [];
+const token = '818392022:AAEuHp2dFA5MYcC7r_NQHobsFzG8Z4DM2ow';
+const groupId = '-430179139';
+let isFirstRender = false;
 
 $.get(SETTING_ENDPOINT, (resp) => {
   const setting = resp.mobile.south;
@@ -31,6 +34,10 @@ $.get(SETTING_ENDPOINT, (resp) => {
       if (count === collectionCount) {
         renderCollections();
       }
+      if (!isFirstRender) {
+        isFirstRender = true;
+        renderProductsByCollection(col.col_id);
+      }
     });
   });
 });
@@ -47,8 +54,14 @@ function renderCollections() {
   $('#menu').html(html);
 }
 
-function renderProductsByCollection() {
-  const collectionId = $(this).data('id');
+function renderProductsByCollection(id) {
+  let collectionId;
+  if (typeof id === 'string') {
+    collectionId = id;
+  } else {
+    collectionId = $(this).data('id');
+  }
+  console.log(collectionId);
   const products = productHash[collectionId];
   const productsData = products.map((p) => ({
     id: p.id,
@@ -90,7 +103,7 @@ function addProductInCart() {
   const parent = $(this).closest('div');
   const id = $(parent).data('id');
   const existItem = cart.find((c) => c.item.id === id);
-  existItem.quantity+=1;
+  existItem.quantity += 1;
   renderCart();
 }
 
@@ -110,8 +123,8 @@ function removeProductInCart() {
   }
 }
 
-function remvoveAll(){
-  console.log('remove all')
+function remvoveAll() {
+  console.log('remove all');
   cart = [];
   renderCart();
 }
@@ -121,10 +134,43 @@ function renderCart() {
   $('#cart').html(html);
 }
 
-function renderPay(){
+function renderPay() {
   const html = tmpl('tpl-payment', cart);
-  console.log(cart)
-  $('#payment').html(html);
+  $('#payment-outer').html(html);
+  $('.hover_bkgr_fricc').show();
+  const cb = (err, res) => {
+    if (err) {
+      alert('Khong the gui tin nhan');
+    } else {
+      alert('Dat hang thanh toan');
+    }
+  };
+  let message = 'Trần Thanh Quang\n---------\n';
+  message += cart
+    .map((c) => {
+      return `${c.item.name} x ${c.quantity} = ${(c.item.minPrice * c.quantity + '').formatMoney()}\n`;
+    })
+    .join('');
+  message += `--------\nTổng tiền : ${(cart.reduce((cur, i) => cur + i.quantity * i.item.minPrice, 0) + '').formatMoney()}`;
+  sendMessage(groupId, message, token, cb);
+}
+
+function sendMessage(id, message, token, cb) {
+  var url = `https://api.telegram.org/bot${token}/sendMessage`;
+  var data = {
+    chat_id: id,
+    text: message,
+  };
+
+  $.post(url, data)
+    .then((resp) => {
+      console.log('send message success:', resp);
+      cb(null, resp);
+    })
+    .fail((error) => {
+      console.error('send message error:', error);
+      cb(error, null);
+    });
 }
 
 String.prototype.formatMoney = function () {
@@ -136,6 +182,11 @@ $(document).on('click', '.item-mn-product-action', addProductToCart);
 $(document).on('click', '.btn-action.add', addProductInCart);
 $(document).on('click', '.btn-action.remove', removeProductInCart);
 $(document).on('click', '#remove-all', remvoveAll);
-$(document).on('click', '#pay', renderPay);
-
 $(document).on('click', '.trigger_popup_fricc', renderPay);
+$(document).on('click', '.hover_bkgr_fricc', function () {
+  $('.hover_bkgr_fricc').hide();
+});
+
+$(document).on('click', '.popupCloseButton', function () {
+  $('.hover_bkgr_fricc').hide();
+});
